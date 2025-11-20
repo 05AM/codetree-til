a93948f8705a38c31bf0487cefd013dd4f39189f
이찬미 <anytime0224@gmail.com>
2025-11-20 23:52:09 +0900

    Title: 흰검 칠하기, Time: 223ms, Memory: 15MB, Status: Passed - Codetree

--- 251120/흰검 칠하기/painting-white-black.java
+++ 251120/흰검 칠하기/painting-white-black.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 public class Main {
 
-    static final int BASIC = 0;
-    static final int WHITE = 2;
-    static final int BLACK = -2;
-    static final int GRAY = 1;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int offset = 100 * N;
 
-        int[] result = new int[2 * offset];
-        int[] overlap = new int[2 * offset];
+        int[] whiteCnt = new int[2 * offset];
+        int[] blackCnt = new int[2 * offset];
+        char[] lastColor = new char[2 * offset];
+
         int curr = offset;
 
         for (int i = 0; i < N; i++) {
@@ -20,12 +17,12 @@ public class Main {
             char d = sc.next().charAt(0);
 
             while (x-- > 0) {
-                if (overlap[curr] >= 3) {
-                    overlap[curr] += 1;
-                    result[curr] = GRAY;
-                } else {
-                    result[curr] = (d == 'L' ? WHITE : BLACK); 
-                    overlap[curr] += 1;
+                if (d == 'L') {
+                    whiteCnt[curr] += 1;
+                    lastColor[curr] = 'W';
+                } else if (d == 'R') {
+                    blackCnt[curr] += 1;
+                    lastColor[curr] = 'B';
                 }
 
                 if (x != 0) {
@@ -38,13 +35,13 @@ public class Main {
         int black = 0;
         int gray = 0;
 
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] == WHITE) {
-                white++;
-            } else if (result[i] == BLACK) {
-                black++;
-            } else if (result[i] == GRAY) {
+        for (int i = 0; i < lastColor.length; i++) {
+            if (whiteCnt[i] >= 2 && blackCnt[i] >=2) {
                 gray++;
+            } else if (lastColor[i] == 'W') {
+                white++;
+            } else if (lastColor[i] == 'B') {
+                black++;
             }
         }
 
