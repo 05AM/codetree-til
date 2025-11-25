814a5b25d3a153d365be290d2ceed84fe34d501c
이찬미 <anytime0224@gmail.com>
2025-11-25 14:25:06 +0900

    Title: 계속 중첩되는 사각형, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 251125/계속 중첩되는 사각형/continuously-overlapping-squares.java
+++ 251125/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -20,7 +20,7 @@ public class Main {
 
             for (int x = x1; x < x2; x++) {
                 for (int y = y1; y < y2; y++) {
-                    board[x][y] = 1;
+                    board[x][y] = value;
                 }
             }
         }
