24932889ff1f4c328f8b6aeb2dbda67e44aae07f
이찬미 <anytime0224@gmail.com>
2025-12-02 16:45:08 +0900

    Title: 좌우로 움직이는 로봇, Time: 135ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 251202/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class Main {
+
+    private static final int OFFSET = 50000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int[] a = new int[OFFSET + 1];
+        Arrays.fill(a, OFFSET);
+
+        a[0] = 0;
+        int aTime = 1;
+        int aLoc = 0;
+
+        for (int i = 0; i < n; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            while (t-- > 0) {
+                int v = d == 'R' ? 1 : -1;
+                aLoc += v;
+                a[aTime++] = aLoc;
+            }
+        }
+        
+        int[] b = new int[OFFSET * 2];
+        Arrays.fill(b, OFFSET);
+
+        b[0] = 0;
+        int bTime = 1;
+        int bLoc = 0;
+
+        for (int i = 0; i < m; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            while (t-- > 0) {
+                int v = d == 'R' ? 1 : -1;
+                bLoc += v;
+                b[bTime++] = bLoc;
+            }
+        }
+        
+        int result = 0;
+        // 현재 a와 b의 인덱스 값
+        aLoc = 0;
+        bLoc = 0;
+        for (int i = 1; i <= OFFSET; i++) {
+            if (a[i] == OFFSET && b[i] == OFFSET) {
+                break;
+            }
+
+            int aCurr = a[i] != OFFSET ? i : aLoc;
+            int bCurr = b[i] != OFFSET ? i : bLoc;
+            aLoc = aCurr;
+            bLoc = bCurr;
+
+            // System.out.println("i: " + i + ", a: " + a[i] + ", b: " + b[i] + " " + aLoc + " " + bLoc);
+
+            if (a[aLoc] == b[bLoc]) {
+                if (a[aLoc - 1] != b[bLoc - 1]) {
+                    result++;
+                    // System.out.println("i: " + i + ", a: " + a[i] + ", b: " + b[i] + " " + aLoc + " " + bLoc);
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
