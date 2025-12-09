c734e789cf2c5cc5639243e75980c4bec5510b24
이찬미 <anytime0224@gmail.com>
2025-12-09 11:54:45 +0900

    Title: 작은 구슬의 이동, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251209/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static final int[] dr = new int[] {-1, 0, 0, 1};
+    private static final int[] dc = new int[] {0, 1, -1, 0};
+
+    private static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int T = sc.nextInt();
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        String D = sc.next();
+
+        int dirNum = getDirNum(D);
+        while (T-- > 1) {
+            // System.out.println("dirNum: " + dirNum);
+            // System.out.println("R: " + R);
+            // System.out.println("C: " + C);
+
+            int nextR = R + dr[dirNum];
+            int nextC = C + dc[dirNum];
+
+            if (inRange(nextR, nextC)) {
+                R = nextR;
+                C = nextC;
+            } else {
+                dirNum = 3 - dirNum;
+                R += dr[dirNum];
+                C += dc[dirNum];
+            }
+        }
+
+        System.out.println(R + " " + C);
+    }
+
+    public static int getDirNum(String dir) {
+        if (dir.equals("U")) {
+            return 0;
+        } else if (dir.equals("D")) {
+            return 3;
+        } else if (dir.equals("R")) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public static boolean inRange(int r, int c) {
+        return r >= 1 && r <= N && c >= 1 && c <= N;
+    }
+}
