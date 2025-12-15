1098e92b1c513c0d0340978b564555e1189c53d1
이찬미 <anytime0224@gmail.com>
2025-12-15 12:17:55 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 235ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 251215/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main {
+
+    private static final int[] dr = new int[] {0, 1, 0, -1};
+    private static final int[] dc = new int[] {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        Character[][] grid = new Character[n][m];
+
+        int row = 0;
+        int col = 0;
+        char ch = 'A';
+        int dirNum = 0;
+
+        for (int i = 0; i < n * m; i++) {
+            grid[row][col] = (char) (ch + i);
+
+            int nr = row + dr[dirNum];
+            int nc = col + dc[dirNum];
+
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m || grid[nr][nc] != null) {
+                dirNum = (dirNum + 1) % 4;
+                row += dr[dirNum];
+                col += dc[dirNum];
+            } else { 
+                row = nr;
+                col = nc;
+            }
+        }
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                System.out.print(grid[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
