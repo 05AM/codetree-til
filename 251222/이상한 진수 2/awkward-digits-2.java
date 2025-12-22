5e7ffd07a349700f6d7a96c3bdbd0abde9c53cf6
이찬미 <anytime0224@gmail.com>
2025-12-22 20:02:10 +0900

    Title: 이상한 진수 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251222/이상한 진수 2/awkward-digits-2.java
+++ 251222/이상한 진수 2/awkward-digits-2.java
@@ -5,14 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
 
-        int max = Integer.parseInt(a, 2);
+        int max = 0;
         for (int i = 0; i < a.length(); i++) {
             StringBuilder temp = new StringBuilder(a);
-
-            if (temp.charAt(i) == '0') {
-                temp.setCharAt(i, '1');
-                max = Math.max(max, Integer.parseInt(temp.toString(), 2));
-            }
+            temp.setCharAt(i, '1');
+            max = Math.max(max, Integer.parseInt(temp.toString(), 2));
         }
 
         System.out.println(max);
